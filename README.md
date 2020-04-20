32cf1e43abfb2ff350dee275403bf4d43c8783f5
lihu <hu_li888@foxmail.com>
2020-04-21 00:12:11 +0800

    commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# KLineChartSample
+KLineChart sample code, Contains multiple front-end framework versions.
+
+## Run
+### React
+```bash
+cd react-sample && npm run start
+```
+
+### Vue
+🚧 Under construction
+
+### Angular
+🚧 Under construction
+
+## License
+[MIT](./LICENSE)
---
+++ react-sample/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
