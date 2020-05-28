133f636b1699869a03c219ab6245fe8f288537a8
lihu <hu_li888@foxmail.com>
2020-05-28 14:32:10 +0800

    feat: 新增自定义指标和自定义蜡烛图label示例

--- ng-sample/src/app/app.module.ts
+++ ng-sample/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BasicKLineChartComponent } from './k-line-chart/basic/basic-k-line-char
 import { RealTimeKLineChartComponent } from './k-line-chart/real-time/real-time-k-line-chart.component';
 import { UpdateKLineChartComponent } from './k-line-chart/update/update-k-line-chart.component';
 import { LoadMoreKLineChartComponent } from './k-line-chart/load-more/load-more-k-line-chart.component';
-import { CandleStickTechnicalIndicatorSettingKLineChartComponent } from './k-line-chart/candle-stickte-technical-indicator-setting/candle-stick-technical-indicator-setting-k-line-chart.component';
+import { CandlestickTechnicalIndicatorSettingKLineChartComponent } from './k-line-chart/candlestick-technical-indicator-setting/candle-stick-technical-indicator-setting-k-line-chart.component';
 import { CustomFontKLineChartComponent } from './k-line-chart/custom-font/custom-font-k-line-chart.component';
 import { CustomLanguageKLineChartComponent } from './k-line-chart/custom-language/custom-language-k-line-chart.component';
 import { CustomThemeKLineChartComponent } from './k-line-chart/custom-theme/custom-theme-k-line-chart.component';
@@ -16,6 +16,7 @@ import { FollowCrosshairPromptKLineChartComponent } from './k-line-chart/follow-
 import { LeftYAxisKLineChartComponent } from './k-line-chart/left-y-axis/left-y-axis-k-line-chart.component';
 import { PercentageYAxisKLineChartComponent } from './k-line-chart/percentage-y-axis/percentage-y-axis-k-line-chart.component';
 import { TechnicalIndicatorKLineChartComponent } from './k-line-chart/technical-indicator/technical-indicator-k-line-chart.component';
+import { CustomTechnicalIndicatorKLineChartComponent } from './k-line-chart/custom-technical-indicator/custom-technical-indicator-k-line-chart.component';
 import { TechnicalIndicatorLastDataPromptKLineChartComponent } from './k-line-chart/technical-indicator-last-data-prompt/technical-indicator-last-data-prompt-k-line-chart.component';
 import { TimezoneKLineChartComponent } from './k-line-chart/timezone/timezone-k-line-chart.component';
 import { WithoutYAxisKLineChartComponent } from './k-line-chart/without-y-axis/without-y-axis-k-line-chart.component';
@@ -28,7 +29,7 @@ import { YAxisTextInsideKLineChartComponent } from './k-line-chart/y-axis-text-i
     RealTimeKLineChartComponent,
     UpdateKLineChartComponent,
     LoadMoreKLineChartComponent,
-    CandleStickTechnicalIndicatorSettingKLineChartComponent,
+    CandlestickTechnicalIndicatorSettingKLineChartComponent,
     CustomFontKLineChartComponent,
     CustomLanguageKLineChartComponent,
     CustomThemeKLineChartComponent,
@@ -38,6 +39,7 @@ import { YAxisTextInsideKLineChartComponent } from './k-line-chart/y-axis-text-i
     LeftYAxisKLineChartComponent,
     PercentageYAxisKLineChartComponent,
     TechnicalIndicatorKLineChartComponent,
+    CustomTechnicalIndicatorKLineChartComponent,
     TechnicalIndicatorLastDataPromptKLineChartComponent,
     TimezoneKLineChartComponent,
     WithoutYAxisKLineChartComponent,
---
+++ ng-sample/src/app/k-line-chart/custom-technical-indicator/custom-technical-indicator-k-line-chart.component.ts
@@ -0,0 +1,21 @@
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { init, dispose } from 'klinecharts';
+import { priceAndVolumeTrend } from 'klinecharts-technical-indicator';
+
+import generatedKLineDataList from '../../generatedKLineDataList';
+
+@Component({
+  selector: 'app-custom-technical-indicator-k-kline-chart',
+  templateUrl: './custom-technical-indicator-k-line-chart.component.html',
+})
+export class CustomTechnicalIndicatorKLineChartComponent implements AfterViewInit, OnDestroy {
+  ngAfterViewInit(): void {
+    const kLineChart = init('custom-technical-indicator-k-line');
+    kLineChart.addCustomTechnicalIndicator(priceAndVolumeTrend);
+    kLineChart.applyNewData(generatedKLineDataList());
+  }
+
+  ngOnDestroy(): void {
+    dispose('custom-technical-indicator-k-line');
+  }
+}
